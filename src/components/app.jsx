d544b0ba90f518247a07c96729360375ddc8a2c3
Romain niam0r <niamor@mm.st>
2018-07-05 14:20:23 +0100

    Flat correctly displaying, ! remember react's inline syntax for style=

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
 import FlatList from './flat_list.jsx';
+import Flat from './flat.jsx';
 import flats from '../data/flats.js'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      flats: flats,
+      selectedFlat: null
+    }
+  }
   render() {
     return (
       <div>
         <div className="flat-list">
-          <FlatList flats={flats} />
+          <FlatList flats={this.state.flats} />
+          <Flat flat={this.state.flats[0]}/>
         </div>
         <div className="map-container">
 
--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class Flat extends Component {
   render() {
     return (
-      <div className="card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg';);">
+      <div
+        className="card"
+        style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('${this.props.flat.imageUrl}')` }}
+      >
         <div className="card-category"> 150 EUR</div>
         <div className="card-description">
           <h2>Super 60m2 in trendy neighborhood!</h2>
