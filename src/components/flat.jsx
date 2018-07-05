08eb6e2b55eeac8c8bf15d85a1ba91137f010b71
Romain niam0r <niamor@mm.st>
2018-07-05 14:26:12 +0100

    implemented data display from props and used es6 desctructuring

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 
 export default class Flat extends Component {
   render() {
+    const { imageUrl, price, name } = this.props.flat;
     return (
       <div
         className="card"
-        style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('${this.props.flat.imageUrl}')` }}
+        style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('${imageUrl}')` }}
       >
-        <div className="card-category"> 150 EUR</div>
+        <div className="card-category">{price}</div>
         <div className="card-description">
-          <h2>Super 60m2 in trendy neighborhood!</h2>
+          <h2>{name}</h2>
         </div>
         <a className="card-link" href="#"></a>
       </div>
